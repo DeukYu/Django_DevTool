36ddec3a921af1d9d28310d6de0faad3037beade
DeukYu <ldy8070@naver.com>
2021-10-20 00:38:06 +0900

    정적 파일 제공 및 페이지 추가

--- DevTool/DevTool/urls.py
+++ DevTool/DevTool/urls.py
@@ -1,8 +1,11 @@
 from django.conf.urls import include, url
-import HelloDjangoApp.views
+import DevToolApp.views
 
 # Django processes URL patterns in the order they appear in the array
+# r 접두사 "원시" 문자열 내의 문자를 이스케이프하지 않도록 지시
+# ^ "줄의 시작 / $ "줄의 끝"
 urlpatterns = [
-    url(r'^$', HelloDjangoApp.views.index, name='index'),
-    url(r'^home$', HelloDjangoApp.views.index, name='home'),
+    url(r'^$', DevToolApp.views.index, name='index'),
+    url(r'^home$', DevToolApp.views.index, name='home'),
+    url(r'^about$', DevToolApp.views.about, name='about'),
 ]
